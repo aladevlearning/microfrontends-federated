14a5ae70712cd3eda7abf595f4a7658e9e33da0b
ala <ala@zmags.com>
2021-11-22 00:29:37 +0100

    module federated react

--- mfe-accounts/webpack.prod.js
+++ mfe-accounts/webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common.js').default;
 
 module.exports = merge(common, {
     mode: 'production'
